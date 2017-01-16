6fad5104e75a254aa90cb7c1eff7de0c63bfdb84
Clark Sirl <clark@clarksirl.co.uk>
2017-01-16 15:04:37 +0000

    Added additional handler for "newton"

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("newton")) {
+            return "Sir Isaac Newton (4 January 1643 - 31 March 1727) was an English mathematician, astronomer," +
+                    " and physicist who is widely recognised as one of the most influential scientists of all time and" +
+                    " a key figure in the scientific revolution.";
+        }
         return "";
     }
 }
